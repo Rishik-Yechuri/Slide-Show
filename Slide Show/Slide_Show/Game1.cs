5097c6e3c6da1405c0760ecbdfd394272cb87a42
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2021-02-04 22:30:31 -0800

    Add project files.

---
+++ Slide Show/Slide_Show/Game1.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
+
+namespace Slide_Show
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        int current;
+        List<Rectangle> rects;
+        Texture2D spriteSheet;
+        Rectangle screenRct;
+        Rectangle window;
+
+        int time;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            int screenWidth = graphics.GraphicsDevice.Viewport.Width;
+            int screenHeight = graphics.GraphicsDevice.Viewport.Height;
+            window = new Rectangle(0, 0, screenWidth, screenHeight);
+            current = 0;
+            time = 0;
+            rects = new List<Rectangle>();
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            spriteSheet = this.Content.Load<Texture2D>("sprite sheet");
+
+            ReadInts(@"Content/rectangle data.txt");
+        }
+
+        private void ReadInts(string path)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        string[] parts = line.Split(' ');
+                        rects.Add(new Rectangle(Convert.ToInt32(parts[0]),
+                            Convert.ToInt32(parts[1]), Convert.ToInt32(parts[2]),
+                            Convert.ToInt32(parts[3])));
+                    }
+                }
+            }
+            catch (Exception e){ }
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            // TODO: Add your update logic here
+            time++;
+            if ( time == 120)
+            {
+                current++;
+                time = 0;
+            }
+
+            if (current == rects.Count) { 
+                current = 0;
+            }
+
+            screenRct = new Rectangle((window.Width / 2) - (rects[current].Width / 2),(window.Height / 2) - (rects[current].Height / 2), rects[current].Width, rects[current].Height);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+            spriteBatch.Begin();
+            // TODO: Add your drawing code here
+            spriteBatch.Draw(spriteSheet, screenRct, rects[current], Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
